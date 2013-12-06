932573105bcfac469c263fc0dcdaeac804514507
Leonardo Graterol (Pankas) <pankas87@gmail.com>
2013-12-05 22:12:31 -0430

    User Model, new methods: followedBy, likedBy

--- app/models/User.php
+++ app/models/User.php
@@ -56,7 +56,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	public function liked_pictures()
 	{
-		return $this->belongsToMany('Picture');
+		return $this->belongsToMany('Picture')->withPivot('created_at', 'updated_at');
 	}
 
 	public function comments()
@@ -76,13 +76,35 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	public function getTimelinePictures()
 	{
+		$pictures 			= Picture::with('user')->where('user_id', '=', Auth::user()->id);		
 		$following_ids	= $this->following()->select('user_id')->get()->all();
 
-		foreach($following_ids as $key => &$val)
-			$val = $val->user_id;
+		if(! empty($following_ids))
+		{
+			foreach($following_ids as $key => &$val)
+				$val = $val->user_id;
 
-		$pictures	= Picture::whereIn('user_id', $following_ids )->orWhere('user_id', '=', Auth::user()->id)->orderBy('created_at', 'DESC')->get()->all();
+			$pictures	= $pictures->orWhereIn('user_id', $following_ids );
+		}
+		
+		$pictures = $pictures->orderBy('created_at', 'DESC')->get()->all();
 
 		return $pictures;
+	}
+
+	public function followedBy($follower_id)
+	{		
+		if( $this->followers()->where('follower_id', '=', $follower_id)->get()->first() === null )
+			return false;
+		else
+			return true;
+	}
+
+	public function likesPicture($picture_id)
+	{
+		if( $this->liked_pictures()->where('picture_id', '=', $picture_id)->get()->first() === null )
+			return true;
+		else
+			return false;
 	}
 }

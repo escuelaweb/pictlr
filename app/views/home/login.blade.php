0b0dec20ba35b10ba2cd6962ba23ce121b0a9816
Leonardo Graterol (Pankas) <pankas87@gmail.com>
2013-12-31 00:18:35 -0430

    Login page: New layout and design, taking advantage of Bootstrap warnings and labels with warning format

--- app/views/home/login.blade.php
+++ app/views/home/login.blade.php
@@ -1,12 +1,52 @@
-@extends('layouts.master')
+@extends('layouts.external')
 
-@section('title')
-Pictlr - Login
-@stop
+@section('title') Pictlr - Inicio de Sesión @stop
 
-@section('header')
-<div class="row">
-	<h1>Pictlr - Login</h1>
+@section('content')
+<div class="container">
+	<div class="row">
+		<h1 class="col-md-6 col-md-offset-3">Bienvenido de Vuelta a Pictlr</h1>
+	</div>
+
+	@if(Session::has('message'))
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<p class="alert alert-danger">{{Session::get('message')}}</p>
+		</div>
+	</div>
+	@endif
+
+	{{Form::open( array('url' => '/authenticate', 'method' => 'POST') )}}
+		<fieldset class="form-group row">
+			<div class="col-md-6 col-md-offset-3">
+				{{Form::label('email', 'Email: ')}}
+
+				@if($errors->has('email'))
+				{{Form::label('email', $errors->first('email'), array('class' => 'label label-warning'))}}
+				@endif
+
+				{{Form::text('email', null, array('class' => 'form-control'))}}
+			</div>
+		</fieldset>
+		<fieldset class="form-group row">
+			<div class="col-md-6 col-md-offset-3">
+				{{Form::label('password', 'Contraseña: ')}}
+
+				@if($errors->has('password'))
+				{{Form::label('password', $errors->first('password'), array('class' => 'label label-warning'))}}
+				@endif
+
+				{{Form::password('password', array('class' => 'form-control'))}}
+			</div>
+		</fieldset>		
+		<fieldset class="form-group row">
+			<div class="col-md-6 col-md-offset-3">
+				<input type="submit" class="btn btn-primary" value="Iniciar Sesión" />
+			</div>
+		</fieldset>
+	{{Form::close()}}
+	
+
 </div>
 @stop
 
